5e4ee74bc542290f59bb42a8a34fdb0684f3cff2
rachid elaid <rachidelaid@gmail.com>
2022-03-08 17:38:53 +0100

    add blue theme

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,46 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/nav.css';
 
-const Nav = ({ title, link }) => (
-  <nav className="navbar">
-    <Link to={link}>
-      <svg viewBox="0 0 24 24">
+const Nav = ({ title, link }) => {
+  const [pink, setPink] = useState(true);
+
+  const changeTheme = () => {
+    if (!pink) {
+      document.documentElement.style.setProperty('--pink-primary', '#ec4c8a');
+      document.documentElement.style.setProperty('--pink-light', '#fd5193');
+      document.documentElement.style.setProperty('--pink-dark', '#ce4176');
+      document.documentElement.style.setProperty('--pink-darker', '#a32656');
+    } else {
+      document.documentElement.style.setProperty('--pink-primary', '#4369B2');
+      document.documentElement.style.setProperty('--pink-light', '#5687E3');
+      document.documentElement.style.setProperty('--pink-dark', '#3F62A5');
+      document.documentElement.style.setProperty('--pink-darker', '#2D4573');
+    }
+    setPink(!pink);
+  };
+
+  return (
+    <nav className="navbar">
+      <Link to={link}>
+        <svg viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
+          />
+        </svg>
+      </Link>
+      <p>{title}</p>
+      <svg viewBox="0 0 24 24" onClick={changeTheme}>
         <path
           fill="currentColor"
-          d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
+          d="M17.5,12A1.5,1.5 0 0,1 16,10.5A1.5,1.5 0 0,1 17.5,9A1.5,1.5 0 0,1 19,10.5A1.5,1.5 0 0,1 17.5,12M14.5,8A1.5,1.5 0 0,1 13,6.5A1.5,1.5 0 0,1 14.5,5A1.5,1.5 0 0,1 16,6.5A1.5,1.5 0 0,1 14.5,8M9.5,8A1.5,1.5 0 0,1 8,6.5A1.5,1.5 0 0,1 9.5,5A1.5,1.5 0 0,1 11,6.5A1.5,1.5 0 0,1 9.5,8M6.5,12A1.5,1.5 0 0,1 5,10.5A1.5,1.5 0 0,1 6.5,9A1.5,1.5 0 0,1 8,10.5A1.5,1.5 0 0,1 6.5,12M12,3A9,9 0 0,0 3,12A9,9 0 0,0 12,21A1.5,1.5 0 0,0 13.5,19.5C13.5,19.11 13.35,18.76 13.11,18.5C12.88,18.23 12.73,17.88 12.73,17.5A1.5,1.5 0 0,1 14.23,16H16A5,5 0 0,0 21,11C21,6.58 16.97,3 12,3Z"
         />
       </svg>
-    </Link>
-    <p>{title}</p>
-    <svg viewBox="0 0 24 24">
-      <path
-        fill="currentColor"
-        d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z"
-      />
-    </svg>
-  </nav>
-);
+    </nav>
+  );
+};
 
 Nav.propTypes = {
   title: PropTypes.string.isRequired,
