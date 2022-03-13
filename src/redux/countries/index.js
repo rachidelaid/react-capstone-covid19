337c7e1e1d99babbf2b770fe166d67681bdf8603
rachid elaid <rachidelaid@gmail.com>
2022-03-13 23:31:37 +0100

    filter out israel

--- src/redux/countries/index.js
+++ src/redux/countries/index.js
@@ -5,25 +5,26 @@ export const AddAllCountries = (payload) => ({
   payload,
 });
 
-export const fetchCountries = () => (dispatch) => fetch('https://restcountries.com/v3.1/all')
-  .then((response) => response.json())
-  .then((data) => {
-    dispatch({
-      type: ADD_COUNTRIES,
-      payload: data.map((c) => ({
-        name: c.name,
-        region: c.region,
-        code: c.cca2,
-      })),
+export const fetchCountries = () => (dispatch) =>
+  fetch('https://restcountries.com/v3.1/all')
+    .then((response) => response.json())
+    .then((data) => {
+      dispatch({
+        type: ADD_COUNTRIES,
+        payload: data.map((c) => ({
+          name: c.name,
+          region: c.region,
+          code: c.cca2,
+        })),
+      });
     });
-  });
 
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_COUNTRIES:
-      return action.payload;
+      return action.payload.map((c) => c.code !== 'IL');
 
     default:
       return state;
